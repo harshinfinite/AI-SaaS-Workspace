181274ce25bb42fe4a9e14dafff343413281ef82
Harsh <panchalharshcd@gmail.com>
2026-06-28 09:35:16 +0530

    feat(org): add OrgMember model

---
+++ server/models/OrgMember.ts
@@ -0,0 +1,32 @@
+import mongoose, { Document, Schema } from 'mongoose';
+
+interface IOrgMember extends Document {
+  userId: mongoose.Types.ObjectId;
+  orgId: mongoose.Types.ObjectId;
+  role: 'owner' | 'admin' | 'member' | 'viewer';
+}
+
+const orgMemberSchema = new Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    orgId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Organization',
+      required: true,
+    },
+    role: {
+      type: String,
+      enum: ['owner', 'admin', 'member', 'viewer'],
+      default: 'viewer',
+    },
+  },
+  { timestamps: true }
+);
+const OrgMember =
+  mongoose.models.OrgMember ||
+  mongoose.model<IOrgMember>('OrgMember', orgMemberSchema);
+export default OrgMember;
