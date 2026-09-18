8915ca5e0fce6774d193fa788597b4bacbca2471
Harsh <panchalharshcd@gmail.com>
2026-09-18 16:21:58 +0530

    feat(server): attach Socket.io to custom http server

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import next from 'next'
 import express from 'express'
 import { createServer } from 'http'
+import {Server} from 'socket.io'
 
 const app = next({dev:process.env.NODE_ENV !== 'production'})
 const handle = app.getRequestHandler()
@@ -8,10 +9,14 @@ const handle = app.getRequestHandler()
 app.prepare().then(()=>{
     const server = express()
     const httpServer = createServer(server)
+    const io = new Server(httpServer)
     server.all('/*splat',(req,res)=>{
         handle(req,res)
     })
     httpServer.listen(3000,()=>{
         console.log("server running on port 3000")
+    })
+    io.on('connection',(socket)=>{
+        console.log('new user connected',socket.id)
     })
 }).catch((error)=>{console.log('Something went wrong',error)})
